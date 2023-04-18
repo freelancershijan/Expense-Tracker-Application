e1a460b1a4f443816e988df38f38120527f38e00
FreelancerShijan <freelancershijan@gmail.com>
2023-04-19 03:30:17 +0600

    description added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { Toaster } from "react-hot-toast";
 import { RouterProvider } from "react-router-dom";
 import { routes } from "./routes";
-
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <div>
+
+      <Helmet>
+        <title>My Awesome React Website</title>
+        <meta name="description" content="This is an Expense Tracking Software whih Created by Md Shijan Ali using MERN Stack" />
+        <meta property="og:title" content="Expense Tracking Software" />
+        <meta property="og:description" content="This is an Expense Tracking Software whih Created by Md Shijan Ali using MERN Stack" />
+        <meta property="og:image" content="./images/shijan.jpg" />
+      </Helmet>
 
 
       <div className="lg:w-3/4 md:mx-10 mx-5 lg:mx-auto my-10">
