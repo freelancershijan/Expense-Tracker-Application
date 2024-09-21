1c90de9e07798e2a6a07124b4ce52851985003f8
MdShijanAli <freelancershijan@gmail.com>
2024-09-22 00:30:36 +0600

    Add
    Added Loading Component  in Funds and Costs Components

--- src/Dashboard/CostsCategory.js
+++ src/Dashboard/CostsCategory.js
@@ -2,6 +2,7 @@ import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom/dist';
 import AddCostCategory from '../Categories/AddCostCategory';
 import BoxItem from '../Components/common/BoxItem';
+import BoxLoading from '../Components/Loading/BoxLoading';
 import { AuthContext } from '../Context/AuthProvider';
 import { useGetUserCostCategoriesQuery } from '../features/costs/costsAPI';
 
@@ -19,16 +20,21 @@ const CostsCategory = () => {
     return (
         <div>
             {
-                costCategories?.results?.data?.length === 0 ? <div className='h-[100vh] px-6 flex items-center justify-center'>
-                    <h1 className='md:text-2xl sm:text-xl text-lg text-center font-semibold'>You Have not any Cost Category. Please Create a Cost Category FIrst</h1>
-                </div> :
-                    <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 justify-center m-10">
-                        {
-                            costCategories?.results?.data?.map(category => <Link key={category._id} to={`/${ category?.type }/${ category?.name }`}>
-                                <BoxItem bg="#FEE8E2" color="red-500" title={category?.name} value={category?.money} isLoading={isLoading} />
-                            </Link>)
-                        }
+                isLoading ?
+                 <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 justify-center m-10">
+                    <BoxLoading value="6" />
                     </div>
+                    :
+                    costCategories?.results?.data?.length === 0 ? <div className='h-[100vh] px-6 flex items-center justify-center'>
+                        <h1 className='md:text-2xl sm:text-xl text-lg text-center font-semibold'>You Have not any Cost Category. Please Create a Cost Category FIrst</h1>
+                    </div> :
+                        <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 justify-center m-10">
+                            {
+                                costCategories?.results?.data?.map(category => <Link key={category._id} to={`/${ category?.type }/${ category?.name }`}>
+                                    <BoxItem bg="#FEE8E2" color="red-500" title={category?.name} value={category?.money} isLoading={isLoading} />
+                                </Link>)
+                            }
+                        </div>
             }
 
             <div className='fixed bottom-10 right-0 flex items-center gap-2'>
--- src/Dashboard/FundsCategory.js
+++ src/Dashboard/FundsCategory.js
@@ -2,6 +2,7 @@ import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom/dist';
 import AddFundCategory from '../Categories/AddFundCategory';
 import BoxItem from '../Components/common/BoxItem';
+import BoxLoading from '../Components/Loading/BoxLoading';
 import { AuthContext } from '../Context/AuthProvider';
 import { useGetUserFundCategoriesQuery } from '../features/funds/fundsAPI';
 
@@ -20,16 +21,21 @@ const FundsCategory = () => {
     return (
         <div>
             {
-                fundCategories?.results?.data?.length === 0 ? <div className='h-[100vh] px-6 flex items-center justify-center'>
-                    <h1 className='md:text-2xl sm:text-xl text-lg text-center font-semibold'>You Have not any Fund Category. Please Create a Fund Category FIrst</h1>
-                </div> :
+                isLoading ?
                     <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 justify-center m-10">
-                        {
-                            fundCategories?.results?.data?.map(category => <Link key={category._id} to={`/${ category?.type }/${ category?.name }`}>
-                                <BoxItem bg="#E5F8ED" color="green-500" title={category?.name} value={category?.money} isLoading={isLoading} />
-                            </Link>)
-                        }
+                        <BoxLoading value="6" />
                     </div>
+                    :
+                    fundCategories?.results?.data?.length === 0 ? <div className='h-[100vh] px-6 flex items-center justify-center'>
+                        <h1 className='md:text-2xl sm:text-xl text-lg text-center font-semibold'>You Have not any Fund Category. Please Create a Fund Category FIrst</h1>
+                    </div> :
+                        <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 justify-center m-10">
+                            {
+                                fundCategories?.results?.data?.map(category => <Link key={category._id} to={`/${ category?.type }/${ category?.name }`}>
+                                    <BoxItem bg="#E5F8ED" color="green-500" title={category?.name} value={category?.money} isLoading={isLoading} />
+                                </Link>)
+                            }
+                        </div>
             }
 
             <div className='fixed bottom-10 right-0 flex items-center gap-2'>
