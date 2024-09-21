50ef2f4e9f54561d5af950c77e95f0f4a70e36c9
MdShijanAli <freelancershijan@gmail.com>
2024-09-21 23:39:07 +0600

    Update
    Costs Categories Module Update

--- src/Dashboard/CostsCategory.js
+++ src/Dashboard/CostsCategory.js
@@ -1,51 +1,48 @@
-import React, { useContext } from 'react';
-import { BiMoney } from 'react-icons/bi';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom/dist';
 import AddCostCategory from '../Categories/AddCostCategory';
+import BoxItem from '../Components/common/BoxItem';
 import { AuthContext } from '../Context/AuthProvider';
+import { useGetUserCostCategoriesQuery } from '../features/costs/costsAPI';
 
 const CostsCategory = () => {
-    const { categories } = useContext(AuthContext);
-    const cost = categories.filter(ctg => ctg.type === 'cost');
+    const { user } = useContext(AuthContext);
+    const [page, setPage] = useState(1);
+    const [limit, setLimit] = useState(20);
+
+    const { data: costCategories, isError, isLoading } = useGetUserCostCategoriesQuery({
+        email: user?.email,
+        page,
+        limit
+    });
+
     return (
         <div>
             {
-                cost.length === 0 ? <div className='h-[100vh] px-6 flex items-center justify-center'>
+                costCategories?.results?.data?.length === 0 ? <div className='h-[100vh] px-6 flex items-center justify-center'>
                     <h1 className='md:text-2xl sm:text-xl text-lg text-center font-semibold'>You Have not any Cost Category. Please Create a Cost Category FIrst</h1>
                 </div> :
-                       <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 justify-center m-10">
-                       {
-                           cost.map(fctg => <Link key={fctg._id} to={`/${fctg?.type}/${fctg?.name}`}> <div className="lg:col-span-1 md:col-span-1 p-5 flex justify-start gap-5 items-center  bg-white rounded-lg shadow-lg">
-       
-                               <div className="bg-[#FEE8E2] rounded-full p-3">
-                                   <BiMoney className="w-6 h-6 text-red-500"></BiMoney>
-       
-                               </div>
-                               <div className="">
-                                   <div className="text-lg font-semibold text-gray-800">{fctg.value.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</div>
-                                   <p className="text-gray-700">{fctg.name}</p>
-                               </div>
-                           </div>
-                           </Link>)
-                       }
-       
-       
-                   </div>
-}
-         
+                    <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 justify-center m-10">
+                        {
+                            costCategories?.results?.data?.map(category => <Link key={category._id} to={`/${ category?.type }/${ category?.name }`}>
+                                <BoxItem bg="#FEE8E2" color="red-500" title={category?.name} value={category?.money} isLoading={isLoading} />
+                            </Link>)
+                        }
+                    </div>
+            }
 
             <div className='fixed bottom-10 right-0 flex items-center gap-2'>
-               <Link to='/delete-category'>
-               <div className='bg-red-700  group w-12 h-12 flex items-center justify-center rounded-full'>
-                   <i className='pi pi-trash text-white'></i>
+                <Link to='/delete-category'>
+                    <div className='bg-red-700  group w-12 h-12 flex items-center justify-center rounded-full'>
+                        <i className='pi pi-trash text-white'></i>
                     </div>
-                    </Link>
+                </Link>
                 <label htmlFor="cost-category-modal">
-                  <div className='bg-green-700 group cursor-pointer w-12 h-12 flex items-center justify-center rounded-full'>
-                   <i className='pi pi-plus text-white'></i>
+                    <div className='bg-green-700 group cursor-pointer w-12 h-12 flex items-center justify-center rounded-full'>
+                        <i className='pi pi-plus text-white'></i>
                     </div>
-                    </label>
-               
+                </label>
+
             </div>
 
             <AddCostCategory />
--- src/features/funds/fundsAPI.js
+++ src/features/funds/fundsAPI.js
@@ -2,13 +2,10 @@ import { apiSlice } from "../api/apiSlice";
 
 export const fundsApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    getUserDetails: builder.query({
-      query: (email) => `/user-details?user=${email}`
-    }),
     getUserFundCategories: builder.query({
       query: ({email, page=1, limit=20}) => `funds/user-all-fund-category/lists?user=${email}&page=${page}&limit=${limit}`
     })
   })
 })
 
-export const { useGetUserDetailsQuery, useGetUserFundCategoriesQuery } = fundsApi;
+export const { useGetUserFundCategoriesQuery } = fundsApi;
