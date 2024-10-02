eb344061a3f2369886f11b048a6f0db455b2bce8
MdShijanAli <freelancershijan@gmail.com>
2024-10-03 00:47:04 +0600

    Add
    Date Filter Implement

--- src/Components/common/BaseDatePicker.js
+++ src/Components/common/BaseDatePicker.js
@@ -5,38 +5,38 @@ import DatePicker from "react-tailwindcss-datepicker";
 import { setDate } from "../../features/filters/filterSlice";
 
 const BaseDatePicker = () => { 
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
     const [value, setValue] = useState({ 
         startDate: null, 
         endDate: null
     });
 
-     // Function to format date to YYYY-MM-DD
-     const formatDate = (date) => {
+    // Function to format date to YYYY-MM-DD
+    const formatDate = (date) => {
         return date ? format(new Date(date), 'yyyy-MM-dd') : "";
     };
 
-    useEffect(()=> {
+    useEffect(() => {
+        // Dispatch the action even if dates are cleared
         dispatch(setDate({
-            start_date: formatDate(value.startDate),
-            end_date: formatDate(value.endDate),
-        }))
-    }, [dispatch, setDate])
+            start_date: formatDate(value?.startDate),
+            end_date: formatDate(value?.endDate),
+        }));
+    }, [value, dispatch]);
 
     // Logging the formatted dates
     console.log('Formatted Value', {
-        startDate: formatDate(value.startDate),
-        endDate: formatDate(value.endDate)
+        startDate: formatDate(value?.startDate),
+        endDate: formatDate(value?.endDate)
     });
-    
 
     return (
-        <div classNames="border-2 border-primary">
+        <div className="border-2 border-primary">
           <DatePicker 
             primaryColor={"blue"}
             value={value} 
             onChange={newValue => setValue(newValue)}
-        /> 
+          /> 
         </div>
     );
 };
